847450e1655f8557d296a7376f381f39661fc22d
chun man poon <chunman906@hotmail.com>
2022-12-18 22:44:11 +0000

    add JS

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// const name = 'Man Poon';
+// const phone = 123456;
+
+// const items = ['pen', 'orange', 999, false]
+
+// items[4] = 'pencil'
+
+// items.push('game')
+
+// items.unshift(123)
+
+// console.log(items.indexOf('pen'));
