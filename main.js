619f95b9fb1dded9bb1dd73fecf33134f8b05379
chun man poon <chunman906@hotmail.com>
2023-01-01 23:28:57 +0000

    add links

--- main.js
+++ main.js
@@ -5,3 +5,34 @@
 // if (value % 2 !== 0){
 //   console.log("Looks good")
 // };
+
+
+let day = 20;
+
+let color = day !== 20 ? "red" : "green" ;
+
+// switch(day){
+//   case 1:
+//     console.log("monday")
+//       break;
+//   case 2:
+//     console.log("tuesday")
+//       break;
+//   case 3:
+//     console.log("wednesday")
+//       break;
+//   case 4:
+//     console.log("thurs")
+//       break;
+//   case 5:
+//     console.log("fri")
+//       break;
+//   case 6:
+//     console.log("sat")
+//       break;
+//   case 7:
+//     console.log("sun")
+//       break;
+//   default:
+//     console.log("invalid")
+// }
